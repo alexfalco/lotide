59be160fd3c97c5d97f3bb48ca42ba8f716dede4
alexfalco <afalconer02@gmail.com>
2019-11-25 18:35:10 -0500

    head

--- head.js
+++ head.js
@@ -1,4 +1,12 @@
 // FUNCTION IMPLEMENTATION
+
+const head = function(arr) {
+  x = arr[0]
+  return x 
+  
+  };
+
+
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
@@ -8,14 +16,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
+
+
+
 // TEST CODE
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-
-
-/*Comparing identical strings
-Comparing non-identical strings
-Comparing identical numbers
-Comparing non-identical numbers */
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 7);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "ello");
