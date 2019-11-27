1a10cd1693ac7acc6ba45200dc37130feb5ae6d2
alexfalco <afalconer02@gmail.com>
2019-11-27 18:38:23 -0500

    findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// findKeyByValue(obj,val)
+const findKeyByValue = function(obj,val) {
+ 
+let keys = (Object.keys(obj))
+
+  for (const item of keys) {
+  
+  if (obj[item] === val) {   // Should add a else??? or will the loop exit at the end 
+  return item }
+  
+  }
+  
+ 
+  }
+  
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
