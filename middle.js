3205b07c547f89546431c6130035130385dd66cd
alexfalco <afalconer02@gmail.com>
2019-12-02 18:40:27 -0500

    adjusted middle

--- middle.js
+++ middle.js
@@ -1,57 +1,69 @@
 
 // // FUNCTION IMPLEMENTATION eqArrays
-// const eqArrays = function(arr1, arr2) {
+// const eqArrays = function (arr1, arr2) {
 
 //   if (arr1.length !== arr2.length) {
 //     return false
 //   }
-//   else { 
-    
+//   else {
+
 //     for (let i = 0; i < arr1.length; i++) {
 //       if (arr1[i] !== arr2[i]) return false;
 //     }
 //     return true;
 //   }
-    
-    
+
+
 // };
 
 
 // // FUNCTION IMPLEMENTATION assertArraysEqual
-// const assertArraysEqual = function(arr1, arr2) {
+// const assertArraysEqual = function (arr1, arr2) {
 
-//   if (eqArrays(arr1,arr2) === true) {
+//   if (eqArrays(arr1, arr2) === true) {
 //     console.log(`Assertion Passed: ${arr1} === ${arr2}`);
 //   }
-//   else { 
-  
+//   else {
+
 //     console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
 //   }
-    
-    
+
+
 // };
 
 
 
 // FUNCTION IMPLEMENTATION middle
 
-const middle = function(arr1) {
-let x = []
-let y = Math.floor(arr1.length/2)
+const middle = function (arr1) {
+  let x = []
+  let y = Math.floor((arr1.length) / 2)
+  let z = arr1.length / 2
+  let z1 = z + 1
+  console.log('z :', z)
+  console.log('z1 :', z1)
 
   if (arr1.length <= 2) {
-     return x
+    console.log('1st')
+    return x
   }
-   else if (arr1.lenght % 2 !== 0) {
+  else if (arr1.length % 2 !== 0) {
+    console.log('2nd')
     x.push(arr1[y])
     return x
-   } 
-   else { 
-    x.push(arr1[y], arr1[y],)
-   
+  }
+  else {
+    console.log('3rd')
+    x.push(arr1[z]-1, arr1[z1]-1)
+    console.log('x :', x)
     return x
   }
+
 }
+
+console.log(middle([1, 2, 3, 4]))
+
+
 
 module.exports = middle
 
